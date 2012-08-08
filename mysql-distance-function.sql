3fab040df353610e3081103e55ec7d8ebd2fbe56
Matt Grundy <matt@mattgrundy.co.uk>
2012-08-08 17:31:25 +0100

    Changed to distance abbreviations

--- mysql-distance-function.sql
+++ mysql-distance-function.sql
@@ -1,9 +1,9 @@
 DROP FUNCTION IF EXISTS GeoDistDiff;
 
 DELIMITER |
-CREATE FUNCTION GeoDistDiff( type ENUM('miles', 'kilometers'), lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT ) RETURNS FLOAT
+CREATE FUNCTION GeoDistDiff( type ENUM('mi', 'km'), lat1 FLOAT, lon1 FLOAT, lat2 FLOAT, lon2 FLOAT ) RETURNS FLOAT
 BEGIN
-  RETURN ( IF(type = 'kilometers', 6371, 3959) * acos( cos( radians(lat2) ) * cos( radians( lat1 ) ) * cos( radians( lon1 ) - radians(lon2) ) + sin( radians(lat2) ) * sin( radians( lat1 ) ) ) );
+  RETURN ( IF(type = 'km', 6371, 3959) * acos( cos( radians(lat2) ) * cos( radians( lat1 ) ) * cos( radians( lon1 ) - radians(lon2) ) + sin( radians(lat2) ) * sin( radians( lat1 ) ) ) );
 END;
 |
 
