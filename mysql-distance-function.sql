a2b9762eaba98bb1fa95eddaa301b29fa99051d7
Nadjib khelifati <nadjib.khelifati@dzmob.net>
2016-02-18 12:52:18 +0100

    Update mysql-distance-function.sql
    Because if distance is > to 1000 this function return 999.9999 => RETURNS DECIMAL(10,7) => max value return is 999.9999

--- mysql-distance-function.sql
+++ mysql-distance-function.sql
@@ -1,7 +1,7 @@
 DROP FUNCTION IF EXISTS GeoDistDiff;
 
 DELIMITER |
-CREATE FUNCTION GeoDistDiff( type ENUM('mi', 'km'), lat1 DECIMAL(10,7), lon1 DECIMAL(10,7), lat2 DECIMAL(10,7), lon2 DECIMAL(10,7) ) RETURNS DECIMAL(10,7)
+CREATE FUNCTION GeoDistDiff( type ENUM('mi', 'km'), lat1 DECIMAL(10,7), lon1 DECIMAL(10,7), lat2 DECIMAL(10,7), lon2 DECIMAL(10,7) ) RETURNS DECIMAL(20,7)
 BEGIN
   RETURN ( IF(type = 'km', 6371, 3959) * acos( cos( radians(lat2) ) * cos( radians( lat1 ) ) * cos( radians( lon1 ) - radians(lon2) ) + sin( radians(lat2) ) * sin( radians( lat1 ) ) ) );
 END;
